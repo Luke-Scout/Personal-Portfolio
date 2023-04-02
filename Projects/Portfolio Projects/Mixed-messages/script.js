41c386acefbe2702d1b955b00288a550d990b454
Luke Bates <bates.h.luke@gmail.com>
2023-04-02 13:40:13 -0600

    Second

--- Projects/Portfolio Projects/Mixed-messages/script.js
+++ Projects/Portfolio Projects/Mixed-messages/script.js
@@ -1,4 +1,51 @@
 function generateNewRandomNumber(num) {
-	return Math.floor(math.random() * num);
+	return Math.floor(Math.random() * num);
 }
-// generates new random number with-in 0-num when called
+
+let messagesObjects = {
+	shouldDo: [
+		"Mow the lawn",
+		"Wash your dog",
+		"Do the dishes",
+		"Mop the floors",
+		"Dust the house",
+		"Get a haircut",
+		"Shave your dog's butt"
+	],
+	haveDone: ["Making breakfast", "Meal prepping", "Grocery shopping"],
+	wantToDo: ["go on a cruise", "go to Rome", "go to Japan"],
+};
+
+let personalMessage = [];
+for (let i = 0; i < Object.keys(messagesObjects).length; i++) {
+	let messageIndex = generateNewRandomNumber(messagesObjects[Object.keys(messagesObjects)[i]].length);
+	let messageItem = Object.keys(messagesObjects)[i];
+	switch (messageItem) {
+		case "shouldDo":
+			personalMessage.push(
+				`You need to ${messagesObjects[messageItem][messageIndex].toLowerCase()} soon.`
+			);
+			break;
+		case "haveDone":
+			personalMessage.push(
+				`Congrats you finished ${messagesObjects[messageItem][messageIndex].toLowerCase()}!`
+			);
+			break;
+		case "wantToDo":
+			personalMessage.push(
+				`You really need to take a break you should ${messagesObjects[messageItem][messageIndex]}.`
+			);
+			break;
+		default:
+			personalMessage.push(
+				"Either something went wrong, or there is not enough information."
+			);
+			break;
+	}
+}
+
+function formatMessage() {
+	let formattedMessage = personalMessage.join(" ");
+	console.log(formattedMessage);
+}
+formatMessage();
